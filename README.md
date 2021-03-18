0ec2a91d48d4199d1c5e4228eab5fcfaa97e0f50
fhmjones <fjones@eoas.ubc.ca>
2021-03-17 17:48:19 -0700

    first success for this kind of dashboard

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Dash-based dashboards
 
-Interactive figures - aiming for similar (simpler) assignment to http://literallycanvas.com/ 
+Interactive annotation of images. Goal is for an assignment similar one we did in 2017 for EOSC 16.326 using http://literallycanvas.com/.
 
+First attempt shows the timescale image and allows placing a "line" shape, erase, and generate PNG figure.
 
+No graph data, axes removed, no zoom or other interaction with the plot.
--- app.py
+++ app.py
@@ -6,6 +6,9 @@
 
 # based on app3.py and ideas at "Dash App With Multiple Inputs" in https://dash.plotly.com/basic-callbacks
 # plotly express line parameters via https://plotly.com/python-api-reference/generated/plotly.express.line.html#plotly.express.line
+# modebar buttons: https://plotly.com/python/configuration-options/ (remove, or add shape-drawing)
+# add text (your name) https://community.plotly.com/t/how-to-add-a-text-area-with-custom-text-as-a-kind-of-legend-in-a-plotly-plot/24349
+
 
 from flask import Flask
 from os import environ
@@ -36,11 +39,24 @@ app.layout = html.Div([
         ----------
         '''),
 
-    html.Label('No. cycles:'),
-    dcc.Slider(id='ncycles', min=2, max=10, value=3, step=0.5,
-        marks={2:'2', 4:'4', 6:'6', 8:'8', 10:'10'}
+    html.Label('Pen thickness (pixels)'),
+    dcc.Slider(id='penthick', min=3, max=15, value=9, step=2,
+        marks={3:'3 pixels', 5:'5', 7:'7', 9:'9', 11:'11', 13:'13', 15:'15'}
         ),        
-    dcc.Graph(id='indicator-graphic'),
+    dcc.Graph(
+        id='indicator-graphic',
+        config={
+                'staticPlot': False,      # True, False
+                'scrollZoom': False,      # True, False
+                'doubleClick': 'reset',   # 'reset', 'autosize' or 'reset+autosize', False
+                'showTips': True,         # True, False
+                'displayModeBar': True,   # True, False, 'hover'
+                'watermark': False,
+                'modeBarButtonsToRemove': ['resetAxis', 'pan2d', 'resetScale2d', 'select2d', 'lasso2d', 'zoom2d', 
+                                            'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'hoverCompareCartesian', 'hoverClosestCartesian'],
+                'modeBarButtonsToAdd': ['drawline', 'eraseshape' ]
+            }
+    ),
 
     dcc.Markdown('''
         ----
@@ -56,35 +72,55 @@ app.layout = html.Div([
 # The callback function with it's app.callback wrapper.
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    Input('ncycles', 'value'),  
+    Input('penthick', 'value'),  
     )    
-def update_graph(ncycles):
+def update_graph(penthick):
     # Create figure
     fig = go.Figure()
 
     # Add trace
-    fig.add_trace(
-        go.Scatter(x=[0, 0.5, 1, 2, 2.2], y=[1.23, 2.5, 0.42, 3, 1])
-    )
+    # fig.add_trace(go.Scatter(x=[0, 0.5, 1, 2, 2.2], y=[1.23, 2.5, 0.42, 3, 1]) )
 
     # Add images
+    # "agerange-exercise1.jpg" is 500 x 340 or 
     fig.add_layout_image(
             dict(
-                source="file://C:/Users/fhmjo/repos/image_annot01/agerange-exercise1.jpg",
-                xref="x",
-                yref="y",
+                source="https://raw.githubusercontent.com/fhmjones/dash_ImageAnnot01/main/assets/agerange-exercise1.jpg",
+                xref="paper",
+                yref="paper",
                 x=0,
-                y=3,
-                sizex=2,
-                sizey=2,
+                y=1,
+                sizex=1,
+                sizey=1,
                 sizing="stretch",
-                opacity=0.5,
-                layer="below")
+                opacity=1,
+                layer="above")
     )
 
     # Set templates
-    fig.update_layout(template="plotly_white")
+    # fig.update_layout(template="plotly_white")
+    fig.update_layout(width=800, height=600)
+    fig.update_layout(annotations=[
+        go.layout.Annotation(
+            text='Your name',
+            align='left',
+            showarrow=False,
+            xref='paper',
+            yref='paper',
+            x=0,
+            y=0,
+        )
+    ])
+    fig.update_xaxes(visible=False)
+    fig.update_yaxes(visible=False)
 
+    # Set dragmode and newshape properties; add modebar buttons
+    fig.update_layout(
+        dragmode='drawline',
+        newshape=dict(line_color='magenta', line_width=penthick),
+        title_text='Click-Drag to draw. Point click to select line before choosing "erase".'
+    )
+    
     return fig
 
 if __name__ == '__main__':
